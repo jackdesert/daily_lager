154b12a19a88c66efb80146764c55d8b38a46cec
Jack Desert <jackdesert@gmail.com>
2014-01-23 16:14:49 -0800

    Output properly generated for YesterdayVerb Verified with mocked specs (no durable storage used)

--- spec/models/verbs/yesterday_verb_spec.rb
+++ spec/models/verbs/yesterday_verb_spec.rb
@@ -1,12 +1,19 @@
 require 'spec_helper'
 
 describe YesterdayVerb do
-  describe '#receive' do
-    let(:words) { '3 miles' }
-    let(:yesterday_verb) { ActionVerb.new(words, Human.new) }
+
+  describe '#process' do
+    let(:ran_today) { Occurrence.new(date: 1, value: 1) }
+    let(:ran_yesterday) { Occurrence.new(date: 2, value: 3) }
+    let(:walked_today) { Occurrence.new(date: 1, value: 5) }
+    let(:walked_yesterday) { Occurrence.new(date: 2, value: 7) }
+    let(:thing1) { Thing.new(name: 'run', default_value: 6, occurrences: [ran_today, ran_yesterday]) }
+    let(:thing2) { Thing.new(name: 'walk', default_value: 2, occurrences: [walked_today, walked_yesterday]) }
+    let(:human) { Human.new(phone_number: '1111111111', things: [thing1, thing2]) }
+    subject { described_class.new('rename run miles', human) }
     it 'returns a message' do
-      mock(yesterday_verb).respond('3 miles entered')
-      yesterday_verb.process
+      mock(subject).respond("Yesterday's totals:\n3 run\n7 walk")
+      subject.send(:process)
     end
   end
 
