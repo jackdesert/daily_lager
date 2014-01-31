a885dde5fed200e7a7db6c5e8d92820e2aac1d9e
Jack Desert <jackdesert@gmail.com>
2014-01-31 14:15:14 -0800

    Completed CreateVerbDefaultValue

--- models/verbs/create_verb_with_default.rb
+++ models/verbs/create_verb_with_default.rb
@@ -1,8 +1,16 @@
 class CreateVerbWithDefault < Verb
 
   def process
-    "#{self.class} not yet supported"
+    if Thing.where(human_id: human.id, name: proposed_thing_name).first
+      "You already have a #{Thing::DISPLAY_NAME} named '#{proposed_thing_name}'"
+    else
+      human.add_thing(name: proposed_thing_name, 
+                      default_value: proposed_thing_default_value)
+      "#{Thing::DISPLAY_NAME.capitalize} '#{proposed_thing_name}' created with a default value of #{proposed_thing_default_value}."
+    end
   end
+
+
 
   private
   def successor
@@ -17,4 +25,14 @@ class CreateVerbWithDefault < Verb
     return false unless words.fourth.match ALL_NUMBERS
     true
   end
+
+  def proposed_thing_name
+    words.second
+  end
+
+  def proposed_thing_default_value
+    words.fourth
+  end
+
+
 end
--- spec/models/verbs/create_verb_with_default_spec.rb
+++ spec/models/verbs/create_verb_with_default_spec.rb
@@ -14,5 +14,43 @@ describe CreateVerbWithDefault do
     verify_appropriateness_of(yesses, described_class)
     verify_inappropriateness_of(nos, described_class)
   end 
+
+
+  describe '#process' do
+    let(:existing_name) { 'run' }
+    let(:thing1) { Thing.new(name: existing_name, default_value: 6) }
+    let(:human) { create(:human) }
+    subject { described_class.new("create #{name} default 3", human) }
+
+    before do
+      human.add_thing(thing1)
+    end
+
+    context 'when the named action does not exist' do
+      let(:name) { 'original' }
+      it 'adds a Thing to the human' do
+        subject.send(:process)
+        human.things.length.should == 2
+        second = human.things.second
+        second.name.should == name
+        human.things.second.default_value.should == 3
+      end
+      it 'returns a message' do
+        subject.send(:process).should == "#{Thing::DISPLAY_NAME.capitalize} 'original' created with a default value of 3."
+      end
+    end
+
+    context 'when the named thing already exists' do
+      let(:name) { existing_name }
+      it 'does not add a Thing to the human' do
+        subject.send(:process)
+        human.things.length.should == 1
+        human.things.first.name.should == name
+      end
+      it 'returns a message' do
+        subject.send(:process).should == "You already have a #{Thing::DISPLAY_NAME} named '#{name}'"
+      end
+    end
+  end
 end
 
