1c7914dd50378ac5c2c2b020e299eca0facef6c9
Jack Desert <jackdesert@gmail.com>
2014-05-11 12:01:55 -0700

    Fixed bug where next and previous did the same thing

--- public/barGraph.js
+++ public/barGraph.js
@@ -37,7 +37,7 @@ $(document).ready(function(){
         }
 
         $('#cycle-previous').on('click', function(){
-            incrementIndex()
+            decrementIndex()
             updateVisible()
         })
 
