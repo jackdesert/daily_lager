a893c780f3528c3693c9cfd70c87885dcb8c4439
Jack Desert <jackdesert@gmail.com>
2014-08-16 22:09:23 +0000

    typing makes stuff show up like it should

--- public/messages.js
+++ public/messages.js
@@ -1,4 +1,8 @@
-function TextController($scope) {
-  $scope.someText = someText
-}
+angular.module('messages', [])
+  .controller('MessageCtrl', function($scope){
+    $scope.data = { newMessage: '' ,
+                    history: function(){ return 'typety type'},
+                    postMessage: function(){ alert('hi') }
+                  }
+  })
 
